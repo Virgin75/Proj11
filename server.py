755dee1c39310d91be4cf87635a82ed0ebd43cb3
Virgin Bitton <virgin.bitton@gmail.com>
2021-06-09 22:42:00 +0200

    bug de mise à jour des points résolu

--- server.py
+++ server.py
@@ -40,7 +40,9 @@ def book(competition, club):
     found_club = [c for c in clubs if c['name'] == club][0]
     found_competition = [c for c in competitions if c['name'] == competition][0]
     if found_club and found_competition:
-        return render_template('booking.html', club=found_club, competition=found_competition)
+        return render_template('booking.html',
+                               club=found_club,
+                               competition=found_competition)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
@@ -51,9 +53,13 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     places_required = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-places_required
+
+    # Update places left in competition and points left of the club
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+    club['points'] = int(club['points']) - places_required
+
     flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, cogmpetitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
